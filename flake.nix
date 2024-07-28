55c58144dc804de45428de1a08d86e75eb89babf
Gautier DI FOLCO <gautier.difolco@gmail.com>
2024-07-28 20:26:28 +0200

    distrib: drop docker (wip)

--- flake.nix
+++ flake.nix
@@ -15,51 +15,13 @@
       let
         pkgs = import nixpkgs { inherit system; };
           hlib = pkgs.haskell.lib.compose;
-          haskellPackages= pkgs.haskell.packages.ghc982.override {
+          haskellPackages = pkgs.haskell.packages.ghc982.override {
             overrides = import "${inputs.cabal-audit}/nix/haskell-overlay.nix" {inherit hlib;};
           };
-        baseImage = pkgs.dockerTools.buildImage {
-          name = "blackheaven/haskell-security-action";
-          tag = "latest";
-
-          copyToRoot = pkgs.buildEnv {
-            name = "image-root";
-            paths = [
-              pkgs.ghc.out
-              pkgs.gitMinimal.out
-            ];
-            pathsToLink = [ "/bin" "/" ];
-          };
-          config = {
-            Env = [
-              "LOCALE_ARCHIVE=${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive"
-              "LC_TIME=en_US.UTF-8"
-              "LANG=en_US.UTF-8"
-              "LANGUAGE=en"
-              "LC_ALL=en_US.UTF-8"
-              # "GIT_DISCOVERY_ACROSS_FILESYSTEM=1"
-            ];
-            Volumes = { "/repository" = { }; };
-            WorkDir = "/repository";
-          };
-        };
         gas = haskellPackages.callCabal2nix "github-action-scan" ./. { };
       in rec {
         packages.github-action-scan = gas;
-        packages.github-action-scan-image = pkgs.dockerTools.buildImage {
-          name = "blackheaven/haskell-security-action";
-          tag = "latest";
-          fromImage = baseImage;
-
-          copyToRoot = pkgs.buildEnv {
-            name = "image-root";
-            paths = [ (pkgs.haskell.lib.justStaticExecutables gas) ];
-            pathsToLink = [ "/bin" "/" ];
-          };
-          config = {
-            Cmd = [ "/bin/github-action-scan" ];
-          };
-        };
+        packages.github-action-scan-static = pkgs.haskell.lib.justStaticExecutables gas;
 
         defaultPackage = packages.github-action-scan;
 
